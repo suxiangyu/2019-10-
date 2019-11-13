67c47d26340f84ed0d4cd3139695218f049e13ac
suxiangyu <1532688069@qq.com>
2019-11-13 09:45:48 +0800

    修改

--- week7/Day5/企业CRM客户管理系统（客户端静态页面）/client/js/login.js
+++ week7/Day5/企业CRM客户管理系统（客户端静态页面）/client/js/login.js
@@ -19,23 +19,23 @@ $(function () {
             if (data.code == 0) {
                 // 密码正确
                 alert('登录成功，即将为您打开页面', {
-                handled: function () {
-                    // 点击关闭按钮的时候执行
-                    location.href = './index.html';
-                }
-            })
-        // 把权限信息  存储到本地
-        localStorage.setItem('power', data.power);
-        // 把用户名存储在本地
-        localStorage.setItem('username', account);
-    } else {
-        // 密码错误
-        alert('账号或密码错误，请重新输入')
-    }
+                    handled: function () {
+                        // 点击关闭按钮的时候执行
+                        location.href = './index.html';
+                    }
+                })
+                // 把权限信息  存储到本地
+                localStorage.setItem('power', data.power);
+                // 把用户名存储在本地
+                localStorage.setItem('username', account);
+            } else {
+                // 密码错误
+                alert('账号或密码错误，请重新输入')
+            }
         }, (err) => {
-    // 网络错误失败
-    alert('网络错误，请稍后重启');
-    console.log(err)
-})
+            // 网络错误失败
+            alert('网络错误，请稍后重启');
+            console.log(err)
+        })
     })
 })
